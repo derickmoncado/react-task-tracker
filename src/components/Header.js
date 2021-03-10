171fce73e0205f4b413a20f45e5b6c2ed4f3d95a
Derick Moncado <derickmoncado@gmail.com>
2021-03-10 14:10:08 -0800

    Delete icons and functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Button from './Button';
 
-const Header = ({ title }) => {
+const Header = () => {
   const onClick = () => {
     console.log('click');
   }
 
   return (
     <header className="header">
-      <h1>{title}</h1>
-      <Button color='green' text='Hello' onClick={onClick} />
+      <h1>Task Tracker</h1>
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,13 @@
 import React from 'react';
+import Task from './Task';
 
-const tasks = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Food Shopping',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  }
-];
+const Tasks = ({ tasks, onDelete }) => {
 
-const Tasks = () => {
   return (
     <div>
-
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete} />
+      ))}
     </div>
   )
 }
