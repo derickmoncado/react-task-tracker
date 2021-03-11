6c9913facec8e23b541c2009eda5b219d40db5dd
Derick Moncado <derickmoncado@gmail.com>
2021-03-11 14:18:30 -0800

    Added task form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
-import { useState } from 'react';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -31,14 +32,18 @@ function App() {
 
   // Toggle reminder
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+    setTasks(tasks.map((task) =>
+      task.id === id ?
+      { ...task, reminder: !task.reminder } :
+      task));
   }
 
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ?
-        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> 
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
         : 'No tasks to show'
       }
     </div>
---
+++ src/components/AddTask.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useState } from 'react';
+
+const AddTask = () => {
+  const [text, setText] = useState('');
+  const [dat, setDay] = useState('');
+  const [reminder, setReminder] = useState(false);
+
+
+  return (
+    <form className='add-form'>
+      <div className='form-control'>
+        <label>Task</label>
+        <input type='text' placeholder='Add Task' />
+      </div>
+      <div className='form-control'>
+        <label>Day & Time</label>
+        <input type='text' placeholder='Add Day and Time' />
+      </div>
+      <div className='form-control form-control-check'>
+        <label>Set Reminder</label>
+        <input type='checkbox' />
+      </div>
+
+      <input className='btn btn-block' type='submit' value='Save Task' />
+    </form>
+  )
+}
+
+export default AddTask;
