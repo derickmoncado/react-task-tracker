3bea96e4bf6d8e67888b64025fb547766cfec049
Derick Moncado <derickmoncado@gmail.com>
2021-03-11 14:26:00 -0800

    Prettier-ified everything

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 const AddTask = () => {
-  const [text, setText] = useState('');
-  const [dat, setDay] = useState('');
+  const [text, setText] = useState("");
+  const [dat, setDay] = useState("");
   const [reminder, setReminder] = useState(false);
 
-
   return (
-    <form className='add-form'>
-      <div className='form-control'>
+    <form className="add-form">
+      <div className="form-control">
         <label>Task</label>
-        <input type='text' placeholder='Add Task' />
+        <input
+          type="text"
+          placeholder="Add Task"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
       </div>
-      <div className='form-control'>
+      <div className="form-control">
         <label>Day & Time</label>
-        <input type='text' placeholder='Add Day and Time' />
+        <input type="text" placeholder="Add Day and Time" />
       </div>
-      <div className='form-control form-control-check'>
+      <div className="form-control form-control-check">
         <label>Set Reminder</label>
-        <input type='checkbox' />
+        <input type="checkbox" />
       </div>
 
-      <input className='btn btn-block' type='submit' value='Save Task' />
+      <input className="btn btn-block" type="submit" value="Save Task" />
     </form>
-  )
-}
+  );
+};
 
 export default AddTask;
