7248beb9a2a313f9ee19aaa5c4b1c881043ab029
Derick Moncado <derickmoncado@gmail.com>
2021-03-12 15:41:40 -0800

    Added form submission functionality

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ function App() {
     }
   ]);
 
+  // Add Task form
+  const addTask = task => {
+    console.log(task);
+  };
+
   // Delete task
   const deleteTask = id => {
     setTasks(tasks.filter(task => task.id !== id));
@@ -42,7 +47,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
