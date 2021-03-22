40f845b7fbb4c7339a06c0fb96e8bc4c392d8c0f
Derick Moncado <derickmoncado@gmail.com>
2021-03-22 15:39:46 -0700

    Added dynamically changing buttun text for Add btn on form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Button from "./Button";
 
-const Header = ({ onAdd }) => {
+const Header = ({ onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <Button color="green" text="Add" onClick={onAdd} />
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
