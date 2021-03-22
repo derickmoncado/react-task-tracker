7591842f8863be3096334d1c97af1711e588c641
Derick Moncado <derickmoncado@gmail.com>
2021-03-22 15:34:34 -0700

    Added Add btn functionality to hide/show form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import Button from './Button';
+import React from "react";
+import Button from "./Button";
 
-const Header = () => {
-  const onClick = () => {
-    console.log('click');
-  }
-
+const Header = ({ onAdd }) => {
   return (
     <header className="header">
       <h1>Task Tracker</h1>
-      <Button color='green' text='Add' onClick={onClick} />
+      <Button color="green" text="Add" onClick={onAdd} />
     </header>
   );
-}
+};
 
 export default Header;
