c8670bbc0e89b08020c52958ee40034e4992456f
Derick Moncado <derickmoncado@gmail.com>
2021-03-22 15:28:24 -0700

    Added addTask functionality

--- src/App.js
+++ src/App.js
@@ -9,36 +9,38 @@ function App() {
       id: 1,
       text: "Doctors Appointment",
       day: "Feb 5th at 2:30pm",
-      reminder: true
+      reminder: true,
     },
     {
       id: 2,
       text: "Meeting at School",
       day: "Feb 6th at 1:30pm",
-      reminder: true
+      reminder: true,
     },
     {
       id: 3,
       text: "Food Shopping",
       day: "Feb 5th at 2:30pm",
-      reminder: true
-    }
+      reminder: true,
+    },
   ]);
 
   // Add Task form
-  const addTask = task => {
-    console.log(task);
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
   };
 
   // Delete task
-  const deleteTask = id => {
-    setTasks(tasks.filter(task => task.id !== id));
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
   // Toggle reminder
-  const toggleReminder = id => {
+  const toggleReminder = (id) => {
     setTasks(
-      tasks.map(task =>
+      tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
       )
     );
